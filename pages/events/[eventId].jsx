607c2ff6dcada697003fe7ff394df6924202c4fb
Hithesh Kumar <hitkumar11@publicisgroupe.net>
2022-01-07 12:46:22 +0530

    added event-details component

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 function EventdetailPage() {
-    return (
-        <div>
-            EventdetailPage
-        </div>
-    )
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) return <p>No event found</p>;
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
-export default EventdetailPage
+export default EventdetailPage;
